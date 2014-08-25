1bbd63197346428326dec47a77c9dcbfd742e7af
LorrieR <rosenberg@Lawrences-iMac.local>
2014-08-25 12:13:26 -0400

    Unit #2 Project 1
    creating new cars objects

---
+++ Object Creation/Carfactory.swift
@@ -0,0 +1,15 @@
+//
+//  Carfactory.swift
+//  Object Creation
+//
+//  Created by Dr. Lawrence Rosenberg on 2014-08-25.
+//  Copyright (c) 2014 Dr. Lawrence Rosenberg. All rights reserved.
+//
+
+import Foundation
+class CarFactory {
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
