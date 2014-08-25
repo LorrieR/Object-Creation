f6899e06c5ea8a3259265700f164a152a0a337da
Dr. Lawrence Rosenberg <rosenberg@Lawrences-iMac.local>
2014-08-25 11:27:27 -0400

    Initial Commit

---
+++ Object Creation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Object Creation
+//
+//  Created by Dr. Lawrence Rosenberg on 2014-08-25.
+//  Copyright (c) 2014 Dr. Lawrence Rosenberg. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
